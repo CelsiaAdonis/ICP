7c5b6b6a4813c8b9bda1b6c7c08785b2c5bd7004
CelsiaAdonis <spyraj@gmail.com>
2016-06-10 19:59:27 -0400

    user input of chords

---
+++ chordin.m
@@ -0,0 +1,9 @@
+function chordin()
+
+%User interface that asks for two chords.
+%Chords may be of any length from 1-??
+
+%Function must be able to divide and save the notes of the chords
+%individually for further processing.
+
+end
