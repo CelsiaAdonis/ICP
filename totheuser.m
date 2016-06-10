526d10882f850cc1cac374e2e9d739cd234a8b0f
CelsiaAdonis <spyraj@gmail.com>
2016-06-10 18:45:42 -0400

    order of defaults to setting does not match

--- totheuser.m
+++ totheuser.m
@@ -3,8 +3,8 @@ function totheuser(alpha, beta, gamma, delta)
 %user interface for the settings that provides defaults
 %and rules for each.
 
-prompt = {'Enter alpha value where alpha > 0', 'Enter beta where beta > 1',
-    'Enter gamma where gamma > beta', 'Enter delta where delta > 0'};
+prompt = {'Enter alpha value (alpha > 0)', 'Enter gamma (gamma > beta)',
+    'Enter beta (beta > 1)', 'Enter delta (delta > 0)'};
 dlg_title = 'Model Settings';
 defaultans = {'99999', '4', '8', '0.1'};
 
