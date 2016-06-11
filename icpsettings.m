dc6bb155a053160254e23620ff73bbec555a7fdb
CelsiaAdonis <spyraj@gmail.com>
2016-06-11 17:47:21 -0400

    figured out order errors

--- icpsettings.m
+++ icpsettings.m
@@ -3,8 +3,8 @@ function [alpha, beta, gamma, delta] = icpsettings()
 %user interface for the settings that provides defaults
 %and rules for each.
 
-prompt = {'Enter alpha value (alpha > 0)', 'Enter gamma (gamma > beta)',
-    'Enter beta (beta > 1)', 'Enter delta (delta > 0)'};
+prompt = {'Enter alpha value (alpha > 0)', 'Enter beta (beta > 1)',...
+    'Enter gamma (gamma > beta)', 'Enter delta (delta > 0)'};
 dlg_title = 'Model Settings';
 defaultans = {'99999', '4', '8', '0.1'};
 
