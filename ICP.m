835ed8548074f82c8b0585e3aa49c0cf29202a44
CelsiaAdonis <spyraj@gmail.com>
2016-06-10 23:26:36 -0400

    pulling variables from other functions

--- ICP.m
+++ ICP.m
@@ -1,1 +1,20 @@
-function ICP(MIDInote)
+function attraction = ICP()
+
+%Calculates the attraction betwen two notes or chords
+%that are inputed by the user.
+%Goes through a series of mathematical steps to output a single
+%attraction value.
+
+
+%step 1: Grab chords
+userchords = chordin();
+global chord1
+global chord2
+
+%hi = upper(chord1)
+%step 2: Grab settings
+
+
+%step 3: translate chords into MIDI
+
+end
