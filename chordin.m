07759722d0e292808ed0caca3085de6909962e4e
CelsiaAdonis <spyraj@gmail.com>
2016-06-11 18:57:41 -0400

    identifies the root, if any

--- chordin.m
+++ chordin.m
@@ -1,16 +1,16 @@
 function [chord1, chord2, root1, root2] = chordin()
 
-%User interface that asks for two chords.
-%Chords may be of any length from 1-??
+%User interface that asks for two chords that may be of 1-n length.
 %Function must be able to divide and save the notes of the chords
 %individually for further processing.
+%It also identifies the root of the chord, if any.
 
 prompt = {'Input note/chord 1:',...
-    'Does chord 1 have an identifiable root? Y/N',...
+    'What is the root? N if none.',...
     'Input note/chord 2:',...
-    'Does chord 2 have an identifiable root? Y/N'};
+    'What is the root? N if none.'};
 dlg_title = 'ICP';
-defaults = {'B3 D4 G4', 'Y', 'c4 e4 g4', 'Y'};
+defaults = {'B3 D4 G4', 'G4', 'c4 e4 g4', 'c4'};
 
 chords = inputdlg(prompt, dlg_title, 1, defaults, 'on');
 
@@ -18,5 +18,6 @@ chord1 = strsplit(chords{1});
 chord2 = strsplit(chords{3});
 root1 = chords{2};
 root2 = chords{4};
+
 
 end
