7681cb04e4bc00278cf6a66a1d311c6985d4c27c
CelsiaAdonis <spyraj@gmail.com>
2016-06-11 20:23:34 -0400

    figured out a boggeling array

--- ICP.m
+++ ICP.m
@@ -7,7 +7,7 @@
 
 
 %step 1: Grab chords & count number of columns
-[chord1, chord2] = chordin();
+[chord1, chord2, root1, root2] = chordin();
 
 [chorow1, chocol1] = size(chord1);
 [chorow2, chocol2] = size(chord2);
@@ -17,7 +17,8 @@
 [alpha, beta, gamma, delta] = icpsettings();
 
 
-%step 3: translate both chords into MIDI
+%step 3: translate both chords & roots into MIDI
+%Also locates the column/row of the respective roots.
 for note = 1:chocol1
     currentNote = chord1{note};
     midiX(note) = translate(currentNote);
@@ -28,6 +29,41 @@ for note = 1:chocol2
     midiY(note) = translate(currentNote);
 end
 
+if root1 ~= 'N'
+    rootX = translate(root1);
+else
+    rootX = 1
+end
+
+if root2 ~= 'N'
+    rootY = translate(root2);
+else
+    rootY = 1
+end
+
+% midiX
+% midiY
+% rootX
+% rootY
+
+for midinote = 1:chocol1
+    if rootX{1} == midiX{midinote}
+        placeX = midinote;
+    else
+        placeX = 1;
+    end
+end
+
+for midinote = 1:chocol2
+    if rootY{1} == midiY{midinote}
+        placeY = midinote;
+    else
+        placeY = 1;
+    end
+end
+
+%placeX
+%placeY
 
 %step 4: Pitch Distance
 %This table calculates the absolute distance between MIDI designations.
@@ -39,7 +75,7 @@ for noteX = 1:chocol1
         PD(noteX,noteY) = abs(chordX{1}-chordY{1});
     end
 end
-PD
+%PD
 
 
 % step 5: Interval Cycles
@@ -49,7 +85,7 @@ for noteX = 1:chocol1
         IC(noteX, noteY) = 12/(gcd(PD(noteX,noteY),12));
     end
 end
-IC
+%IC
 
 
 %step 6: Voice Leading
@@ -59,20 +95,36 @@ for noteX = 1:chocol1
         VL(noteX, noteY) = alpha/(PD(noteX,noteY)+alpha);
     end
 end
-VL
+%VL
+
 
 %step 7: Interval Cycles & Voice Leading
 %Combines semitone distance with interval cycles by multiplication.
 ICVL = IC .* VL;
-ICVL
+%ICVL
+
 
 %step 8: Root Salience
 %RS1 is an array of 1's, RS2 gives weight to the roots based on beta
 %and gamma.
 RS1 = ones(chocol1, chocol2);
+RS2 = RS1;
+
+RS2(placeX, 1:chocol2) = RS2(placeX, 1:chocol2) * beta;
+RS2(1:chocol1,placeY) = RS2(1:chocol1,placeY) * gamma;
+
+firstsum = sum(RS2);
+secondsum = sum(firstsum);
+
+RS3(1:chocol1, 1:chocol2) = RS2(1:chocol1, 1:chocol2) .* (1/secondsum);
+
+%RS1
+RS2
+RS3
+
+%step 9: Consonance & Dissonance
 
 
-RS1
 
 
 %end
