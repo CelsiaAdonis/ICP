e82fc852f039e0f5e73ca6c88f442195157830c5
Kyle D. Gauder <kgauder@gmail.com>
2016-06-12 14:46:49 -0400

    Added support for cell arrays in diffarray func

--- functions/diffarray.m
+++ functions/diffarray.m
@@ -1,12 +1,18 @@
 function y = diffarray(Array)
 % DIFFARRAY Finds the absolute difference between each value within an array
 % Takes one input as a regular Array. ie [30,20,10]
+% It spits out a regular array
 %
-% Does not work with cell arrays
+% Works with cell arrays!
 %
 % TODO: - have it not repeat calculations
+%
+% Written by: Kyle Gauder
 
 A = Array;
+if iscell(A) == 1
+    A = cell2mat(A);
+end
 [ASizeRow,ASize] = size(A);
 
 for i=1:ASize
