71c9181baeea4bc8c05920764a99f3b8a1de59cf
CelsiaAdonis <spyraj@gmail.com>
2016-06-28 12:54:53 -0400

    had some semantic issues with AND but prevailed

--- con_dis.m
+++ con_dis.m
@@ -5,39 +5,62 @@
 %This functin breaks a chord down to intervals and checks whether
 %the chord is dissonant or consonant.
 
+
+%(a) bring in the chords, calculate semitone distance between notes,
+%and size the resulting arrays up.
+
 % [midiX, midiY] = ICP();
 chord1 = [60, 64, 67];
+%chord1 = [57, 59, 67];
 chord2 = [57, 59, 67];
-
 
 arrayX = diffarray(chord1);
 arrayY = diffarray(chord2);
 
-% arrayX = num2cell(array1)
-% arrayY = num2cell(array2);
-
 [row1,ch1size] = size(arrayX);
 [row2,ch2size] = size(arrayY);
-lengthX = 1:ch1size;
-% lengthY = 1:ch2size;
 
-%(b) check the semitones consonance
+
+%(b) check for consonance & label chord as cons or diss
+
 semi_con = {0,'Y'; 1,'N'; 2,'N'; 3,'Y'; 4,'Y'; 5,'Y'; 6,'N';...
     7,'Y'; 8,'Y'; 9,'Y'; 10,'N'; 11,'N'; 12,'Y'};
 
 [semiRow, semiCol] = size(semi_con);
-
 con = 'Y';
+
 for number = 1:ch1size
     for numb = 1:row1
         for interval = 1:semiRow
             if arrayX(numb, number) == semi_con{interval,1}
                 con = semi_con{interval,2};
                 if con == 'N'
-                    chordX = 'diss'
+                    chordX = 'diss';
                     break
                 else
-                    chordX = 'cons'
+                    chordX = 'cons';
+                end
+            end
+        end
+        if con == 'N'
+            break
+        end                
+    end
+    if con == 'N'
+        break
+    end
+end
+
+for number = 1:ch2size
+    for numb = 1:row2
+        for interval = 1:semiRow
+            if arrayY(numb, number) == semi_con{interval,1}
+                con = semi_con{interval,2};
+                if con == 'N'
+                    chordY = 'diss';
+                    break
+                else
+                    chordY = 'cons';
                 end
             end
         end
@@ -51,7 +74,17 @@ for number = 1:ch1size
 end
 
 
+%(c) label the sequence of chords as CD, DC, SM (cons to diss,
+%diss to cons, or same)
 
-
+if chordX == chordY
+    codi = 'SM';
+elseif (chordX == 'cons' & chordY == 'diss')
+    codi = 'CD';
+elseif (chordX == 'diss' & chordY == 'cons')
+    codi = 'DC';
+else
+    codi = 'error: self-destruct imminent';
+end
 
 %end
